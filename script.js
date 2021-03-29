3eecd75abb7e002c71cdb1ca386fd69d1d09d629
rodrigo <rodrigodavila_00098@hotmail.com>
2021-03-29 03:38:49 -0300

    clear display button working

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 var display = document.querySelector(".calc-display-content")
 var numberKeys = document.querySelectorAll(".calc-number")
 
-numberKeys.forEach(key => {
-    key.addEventListener("click", function(){
-        clickNumber(key.textContent)
-    })
-});
 
-function clickNumber(number)
-{
-    if(display.textContent==0){
-        display.textContent="";
-    }
-
-    if(display.textContent.length<9){
-        display.textContent+=number;
-    }
-
+function displayValue(value) {
+    display.textContent = value;
 }
 
-display.textContent="0"
+function displayAppendValue(value) {
+    if (display.textContent == 0) {
+        display.textContent = "";
+    }
 
+    if (display.textContent.length < 9) {
+        display.textContent += value;
+    }
+}
 
+function clickNumber(number) {
+    displayAppendValue(number);
+}
 
+function start() {
+    numberKeys.forEach(key => {
+        key.addEventListener("click", function () {
+            clickNumber(key.textContent)
+        })
+    });
+    
+    document.querySelector(".calc-clear").addEventListener("click", function () {
+        reset();
+    });
+     
+    reset();
+}
+
+function reset() {
+    displayValue("0");
+}
+
+start();
