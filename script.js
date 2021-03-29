bad68b0dcfc025645bcb3f479d6f34da16361775
rodrigo <rodrigodavila_00098@hotmail.com>
2021-03-29 12:31:58 -0300

    display now shows which operation will be performed

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 var display = document.querySelector(".calc-display-content")
 var numberKeys = document.querySelectorAll(".calc-number")
 var operatorKeys = document.querySelectorAll(".calc-operator")
-
 var previousValue = 0
 var currentOperation = ""
-
 var resetDiplayNextTime = false
-
 var displayCotainsDot = false;
 
 function displayValue(value) {
@@ -14,17 +11,16 @@ function displayValue(value) {
 }
 
 function getDisplayValue() {
-    return display.textContent-0;
+    return display.textContent - 0;
 }
 
 function displayAppendValue(value) {
 
     if (resetDiplayNextTime) {
         resetDisplay()
-        resetDiplayNextTime=false;
+        resetDiplayNextTime = false;
     }
 
-    //console.info(getDisplayValue())
     if (getDisplayValue() == "0" && value != ".") {
         display.textContent = "";
         displayCotainsDot = false;
@@ -40,8 +36,6 @@ function displayAppendValue(value) {
     if (display.textContent.length < 9) {
         display.textContent += value;
     }
-
-    
 }
 
 function clickNumber(number) {
@@ -69,42 +63,48 @@ function start() {
     resetDisplay();
 }
 
-function performOperation(operation) {
-    console.info("previousValue: "+getDisplayValue())    
+function performOperation(operation) {    
+    var operationResult;
     resetDiplayNextTime=true;
-    
-    if (currentOperation == "=") {
-        
-    }
-    
+
     if (currentOperation == "+") {        
-        displayValue(previousValue + getDisplayValue())
+        operationResult=previousValue + getDisplayValue()
     }
     
     if (currentOperation == "-") {
-        displayValue(previousValue - getDisplayValue())
-        
-        
+        operationResult=previousValue - getDisplayValue()              
     }
     if (currentOperation == "x") {
-        displayValue(previousValue * getDisplayValue())
-        
-        
-    }
+        operationResult=previousValue * getDisplayValue()
+    }    
     if (currentOperation == "/") {
         if(getDisplayValue()==0){
             resetCalc()
             displayValue("#¡DIV/0!")
         }else{
-            displayValue(previousValue / getDisplayValue())
+            operationResult=previousValue / getDisplayValue()
         }
     }
-    
-    previousValue = getDisplayValue() - 0
+
+    if(currentOperation==""){
+        console.info("first operation")
+        operationResult=getDisplayValue();
+    }
+        
+    previousValue=operationResult
     currentOperation = operation;
 
+    console.info("operationResult: "+operationResult)
+    if (operation == "=") {
+        currentOperation=""
+        displayValue(operationResult);
+        
+    }else{        
+        displayValue(operation)
+    }
+    
+
 }
-function getResult() { }
 
 function resetCalc() {
     resetDisplay()
